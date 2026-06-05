6d1ce68d50ae8019669cf2f9497cca647e75f3e5
onacamping <onacamping@gmail.com>
2026-06-05 01:11:59 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,18 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    metaImagesPlugin(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-          await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
-          ),
-        ]
-      : []),
+    metaImagesPlugin()
   ],
   resolve: {
     alias: {
